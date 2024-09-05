b4aadd181cb138578daaed305aa6b8d281a40b01
RafaelSantos <87025218+ASLRafa@users.noreply.github.com>
2024-09-05 18:43:23 -0300

    study

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,15 +1,19 @@
-import './colaborador.css'
+import "./colaborador.css";
+import { FaTrash } from "react-icons/fa";
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
-    return (<div className="colaborador">
-        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
-            <img src={colaborador.imagem} alt={colaborador.nome} />
-        </div>
-        <div className="rodape">
-            <h4>{colaborador.nome}</h4>
-            <h5>{colaborador.cargo}</h5>
-        </div>
-    </div>)
-}
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+  return (
+    <div className="colaborador">
+      <FaTrash className="deletar" onClick={aoDeletar}></FaTrash>
+      <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+        <img src={colaborador.imagem} alt={colaborador.nome} />
+      </div>
+      <div className="rodape">
+        <h4>{colaborador.nome}</h4>
+        <h5>{colaborador.cargo}</h5>
+      </div>
+    </div>
+  );
+};
 
-export default Colaborador
+export default Colaborador;
